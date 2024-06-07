392f2b0b12f8f54d00a45f45e99eef903e28f78a
Uatilla <uatillaalmeida@gmail.com>
2024-06-07 21:56:36 +0100

    Signals

--- My_project/srcs/signals/signals.c
+++ My_project/srcs/signals/signals.c
@@ -12,13 +12,33 @@
 
 #include "minishell.h"
 
-void	refresh_front(void)
+
+void	sigint_child_handler(int signo)
 {
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-	//na situacao cat nao pode ter redisplay
+	if (signo == SIGINT)
+	{
+		g_signo = 130;
+		write(1, "\n", 1);
+		return ;
+	}
+	else if (signo == SIGQUIT)
+	{
+		g_signo = 131;
+		write(1, "Quit (core dumped)\n", 19);
+		//printf("Quit (core dumped)\n");
+	}
+}
+
+void	set_child_signals(void)
+{
+	signal(SIGINT, sigint_child_handler); //ctrl+c
+	signal(SIGQUIT, sigint_child_handler); //ctrl+\/
+}
+
+void	set_main_signal(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 }
 
 void	sigint_handler(int signo)
@@ -26,30 +46,32 @@ void	sigint_handler(int signo)
 	if (signo == SIGINT)
 	{
 		g_signo = 130;
-		refresh_front();
-	}
-}
-
-void	sigint_child_handler(int signo)
-{
-	if (signo == SIGINT)
-	{
-		g_signo = 130;
-		printf("oiut\n");
 		write(1, "\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
+		rl_redisplay();
+		//na situacao cat nao pode ter redisplay
 	}
 }
-
-void	set_child_signals(void)
-{
-		signal(SIGINT, sigint_child_handler); //ctrl+c
-}
-
 void	set_signals(void)
 {
 		signal(SIGINT, sigint_handler); //ctrl+c
 		signal(SIGQUIT, SIG_IGN); //ctrl+\/
 		signal(SIGTSTP, SIG_IGN);
+}
+
+void	sigint_heredoc_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		printf("\n");
+		printf("MSG DO SIGNAL HANDLER bash: warning: here-document delimited by end-of-file\n");
+		exit(130);
+	}
+}
+
+void	set_heredoc_signal(void)
+{
+	signal(SIGINT, sigint_heredoc_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
